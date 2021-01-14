1b8383a313d3b139f85319a53ecf597c73be6743
Bianca <biannca.iordache@gmail.com>
2021-01-14 14:15:04 +0000

    step 9: setting up test environment

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -3,6 +3,12 @@ require 'bookmark'
 describe Bookmark do
   describe '.all' do
     it 'returns all bookmarks' do
+      connection = PG.connect(dbname: 'bookmark_manager_test')
+
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://makersacademy.com');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://ruby-doc.org');")
+      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://gitimmersion.com');")
+
       bookmarks = Bookmark.all
 
       expect(bookmarks).to include('http://makersacademy.com')
--- spec/features/viewing_bookmarks_spec.rb
+++ spec/features/viewing_bookmarks_spec.rb
@@ -1,11 +1,15 @@
-feature 'Viewing bookmarks' do
-  scenario 'visiting the index page' do
-    visit('/')
-    expect(page).to have_content 'Bookmark Manager'
-  end
+require 'pg'
 
-  scenario 'see list of bookmarks' do
+feature 'Viewing bookmarks' do
+  scenario 'A user can see bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks VALUES(1, 'http://makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks VALUES(2, 'http://ruby-doc.org');")
+    connection.exec("INSERT INTO bookmarks VALUES(3, 'http://gitimmersion.com');")
+
     visit('/bookmarks')
+
     expect(page).to have_content 'http://makersacademy.com'
     expect(page).to have_content 'http://ruby-doc.org'
     expect(page).to have_content 'http://gitimmersion.com'
